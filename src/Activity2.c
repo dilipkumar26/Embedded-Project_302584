02ee1e1c4cd2fdffa8fe7ca600f24da2841ebe73
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-07-24 16:32:28 +0530

    Update Activity2.c

--- src/Activity2.c
+++ src/Activity2.c
@@ -22,10 +22,8 @@ void InitADC()
  {
      ADMUX&=0xf8;
      ch=ch&0b00000111;
-
      ADMUX|=ch;
      ADCSRA|=(1<<ADSC);
-
      while(!(ADCSRA&(1<<ADIF)))
      ADCSRA|=(1<<ADIF);
      return(ADC);
