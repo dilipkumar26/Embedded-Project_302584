d2e57ceb36a467914565255d49c03e85a0984c3b
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-07-24 16:33:33 +0530

    Add files via upload

---
+++ inc/Activity2.h
@@ -0,0 +1,21 @@
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+#include "Activity2.h"
+
+void Init_ADC();
+
+/**
+ * @brief A funciton to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
+ *
+ * @param ch
+ * @return uint16_t
+ */
+_uint16_t ReadADC(uint8_t ch);
+
+/**
+ * @brief A function to setup timer1 with channel A for pulse width modulation with wave generation mode of 10 bits fast PWM and prescaling of 64.
+ *
+ */
+
+
+#endif // ACTIVITY2_H_INCLUDED
