4eb97b77564b47b6422eaec0b71151e9ef1df21e
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-07-26 11:44:02 +0530

    Add files via upload

---
+++ inc/activity2.h
@@ -0,0 +1,19 @@
+/**
+ * @file activity2.h
+ * @author 260842 - K.Nagasowmika
+ * @brief
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+void InitADC();
+
+uint16_t ReadADC(uint8_t ch);
+
+#endif // ACTIVITY2_H_INCLUDED
