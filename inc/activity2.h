ccd976f65c19e79ba93bc4d27b80c4f549894394
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-07-26 12:02:29 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,14 +1,3 @@
-/**
- * @file activity2.h
- * @author 260842 - K.Nagasowmika
- * @brief
- * @version 0.1
- * @date 2021-04-27
- *
- * @copyright Copyright (c) 2021
- *
- */
-
 #ifndef ACTIVITY2_H_INCLUDED
 #define ACTIVITY2_H_INCLUDED
 
